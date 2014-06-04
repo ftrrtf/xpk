c262c9dc9ef42c2defd645c0ac8c92387cd10a4f
xawee <xawee@yandex.ru>
2014-06-04 15:55:51 +0300

    Created OS.cs

---
+++ Task1/Task1/OS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class OS
+    {
+        public string Path { get; set; }
+
+        public OS(string path=null)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                Path = "/";
+                return;
+            }
+            Path = path;
+        }
+
+        public string Listen(string cmd)
+        {
+            string[] args = cmd.Split(' ');
+            string errorMsg = "Incorrect input.";
+
+            if (args.Count() <= 0) 
+            {
+                return errorMsg;
+            }
+            switch (args[0])
+            {
+                case "cd":
+                    {
+                        return Path;
+                    }
+                case "pwd":
+                    {
+                        return Path;
+                    }
+                default:
+                    {
+                        return errorMsg;
+                    }
+            }
+        }
+    }
+}
--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -8,7 +8,7 @@ namespace Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             
         }
